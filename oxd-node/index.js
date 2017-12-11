3ebf8324726987bd03eda8d9d95c2cb69c692e6f
Meghna Joshi <ldeveloperl1985@gmail.com>
2017-12-11 18:39:11 +0530

    Updated code with eslint style checker

--- oxd-node/index.js
+++ oxd-node/index.js
@@ -1,303 +1,277 @@
-var utilities = require('./utilities/utility');
+const utilities = require('./utility');
 
 /**
  * Exports All the methods
  * @param {object} config - All Global properties
- * @returns methods
  */
-module.exports = function (config) {
-  var module = {};
+module.exports = (config) => {
+  const module = {};
 
   /**
-   * Function to register the site and generate a unique ID for the site. If you are using the oxd-https-extension, you must setup the client.
+   * Function to register the site and generate a unique ID for the site.
+   * If you are using the oxd-https-extension, you must setup the client.
    * @param {object} param - All required properties
-   * @returns {function} callback - Callback response function.
+   * @param {function} callback - Callback response function. It return with two parameters.
+   * {object} err - error response
+   * {object} response - success response
    */
-  module.setup_client = function setup_client(param, callback) {
+  module.setup_client = (param, callback) => {
     // Filtered parameters
-    var data = filterParameters(param);
+    const data = filterParameters(param);
 
     // If url is set the request goes to oxd-https-extension
     if (!!data.url) {
-      utilities.oxdHttpRequest(data.url + '/setup-client', data, function (err, response) {
-        return callback(err, response);
-      });
+      utilities.oxdHttpRequest(`${data.url}/setup-client`, data, (err, response) => callback(err, response));
     } else {
       // If url is not set then request goes to oxd-server
-      utilities.oxdSocketRequest(data.port, data.host, data, 'setup_client', function (err, response) {
-        return callback(err, response);
-      });
+      utilities.oxdSocketRequest(data.port, data.host, data, 'setup_client', (err, response) => callback(err, response));
     }
   };
 
   /**
-   * Function is used to get a token which is sent as an input parameter for other methods when the protect_commands_with_access_token is enabled in oxd-server.
+   * Function is  used to get a token which is sent as an input parameter for other methods when the protect_commands_with_access_token is enabled in oxd-server.
    * @param {object} param - All required properties
-   * @returns {function} callback - Callback response function.
+   * @param {function} callback - Callback response function. It return with two parameters.
+   * {object} err - error response
+   * {object} response - success response
    */
-  module.get_client_token = function get_client_token(param, callback) {
+  module.get_client_token = (param, callback) => {
     // Filtered parameters
-    var data = filterParameters(param);
+    const data = filterParameters(param);
 
     // If url is set the request goes to oxd-https-extension
     if (!!data.url) {
-      utilities.oxdHttpRequest(data.url + '/get-client-token', data, function (err, response) {
-        return callback(err, response);
-      });
+      utilities.oxdHttpRequest(`${data.url}/get-client-token`, data, (err, response) => callback(err, response));
     } else {
       // If url is not set then request goes to oxd-server
-      utilities.oxdSocketRequest(data.port, data.host, data, 'get_client_token', function (err, response) {
-        return callback(err, response);
-      });
+      utilities.oxdSocketRequest(data.port, data.host, data, 'get_client_token', (err, response) => callback(err, response));
     }
   };
 
   /**
    * Function to register the site and generate a unique ID for the site
    * @param {object} param - All required properties
-   * @returns {function} callback - Callback response function.
+   * @param {function} callback - Callback response function. It return with two parameters.
+   * {object} err - error response
+   * {object} response - success response
    */
-  module.register_site = function register_site(param, callback) {
+  module.register_site = (param, callback) => {
     // Filtered parameters
-    var data = filterParameters(param);
+    const data = filterParameters(param);
 
     // If url is set the request goes to oxd-https-extension
     if (!!data.url) {
-      utilities.oxdHttpRequest(data.url + '/register-site', data, function (err, response) {
-        return callback(err, response);
-      });
+      utilities.oxdHttpRequest(`${data.url}/register-site`, data, (err, response) => callback(err, response));
     } else {
       // If url is not set then request goes to oxd-server
-      utilities.oxdSocketRequest(data.port, data.host, data, 'register_site', function (err, response) {
-        return callback(err, response);
-      });
+      utilities.oxdSocketRequest(data.port, data.host, data, 'register_site', (err, response) => callback(err, response));
     }
   };
 
   /**
    * Fucntion to update the site's information with OpenID Provider.
    * @param {object} param - All required properties
-   * @returns {function} callback - Callback response function.
+   * @param {function} callback - Callback response function. It return with two parameters.
+   * {object} err - error response
+   * {object} response - success response
    */
-  module.update_site_registration = function update_site_registration(param, callback) {
+  module.update_site_registration = (param, callback) => {
     // Filtered parameters
-    var data = filterParameters(param);
+    const data = filterParameters(param);
 
     // If url is set the request goes to oxd-https-extension
     if (!!data.url) {
-      utilities.oxdHttpRequest(data.url + '/update-site', data, function (err, response) {
-        return callback(err, response);
-      });
+      utilities.oxdHttpRequest(`${data.url}/update-site`, data, (err, response) => callback(err, response));
     } else {
       // If url is not set then request goes to oxd-server
-      utilities.oxdSocketRequest(data.port, data.host, data, 'update_site_registration', function (err, response) {
-        return callback(err, response);
-      });
+      utilities.oxdSocketRequest(data.port, data.host, data, 'update_site_registration', (err, response) => callback(err, response));
     }
   };
 
   /**
    * Function to get the authorization url that can be opened in the browser for the user to provide authorization and authentication
    * @param {object} param - All required properties
-   * @returns {function} callback - Callback response function.
+   * @param {function} callback - Callback response function. It return with two parameters.
+   * {object} err - error response
+   * {object} response - success response
    */
-  module.get_authorization_url = function get_authorization_url(param, callback) {
+  module.get_authorization_url = (param, callback) => {
     // Filtered parameters
-    var data = filterParameters(param);
+    const data = filterParameters(param);
 
     // If url is set the request goes to oxd-https-extension
     if (!!data.url) {
-      utilities.oxdHttpRequest(data.url + '/get-authorization-url', data, function (err, response) {
-        return callback(err, response);
-      });
+      utilities.oxdHttpRequest(`${data.url}/get-authorization-url`, data, (err, response) => callback(err, response));
     } else {
       // If url is not set then request goes to oxd-server
-      utilities.oxdSocketRequest(data.port, data.host, data, 'get_authorization_url', function (err, response) {
-        return callback(err, response);
-      });
+      utilities.oxdSocketRequest(data.port, data.host, data, 'get_authorization_url', (err, response) => callback(err, response));
     }
   };
 
   /**
    * Function to get access code for getting the user details from the OP. It is called after the user authorizes by visiting the authorization URL.
    * @param {object} param - All required properties
-   * @returns {function} callback - Callback response function.
+   * @param {function} callback - Callback response function. It return with two parameters.
+   * {object} err - error response
+   * {object} response - success response
    */
-  module.get_tokens_by_code = function get_tokens_by_code(param, callback) {
+  module.get_tokens_by_code = (param, callback) => {
     // Filtered parameters
-    var data = filterParameters(param);
+    const data = filterParameters(param);
 
     // If url is set the request goes to oxd-https-extension
     if (!!data.url) {
-      utilities.oxdHttpRequest(data.url + '/get-tokens-by-code', data, function (err, response) {
-        return callback(err, response);
-      });
+      utilities.oxdHttpRequest(`${data.url}/get-tokens-by-code`, data, (err, response) => callback(err, response));
     } else {
       // If url is not set then request goes to oxd-server
-      utilities.oxdSocketRequest(data.port, data.host, data, 'get_tokens_by_code', function (err, response) {
-        return callback(err, response);
-      });
+      utilities.oxdSocketRequest(data.port, data.host, data, 'get_tokens_by_code', (err, response) => callback(err, response));
     }
   };
 
   /**
    * Function to get access code for using refresh token
    * @param {object} param - All required properties
-   * @returns {function} callback - Callback response function.
+   * @param {function} callback - Callback response function. It return with two parameters.
+   * {object} err - error response
+   * {object} response - success response
    */
-  module.get_access_token_by_refresh_token = function get_access_token_by_refresh_token(param, callback) {
+  module.get_access_token_by_refresh_token = (param, callback) => {
     // Filtered parameters
-    var data = filterParameters(param);
+    const data = filterParameters(param);
 
     // If url is set the request goes to oxd-https-extension
     if (!!data.url) {
-      utilities.oxdHttpRequest(data.url + '/get-access-token-by-refresh-token', data, function (err, response) {
-        return callback(err, response);
-      });
+      utilities.oxdHttpRequest(`${data.url}/get-access-token-by-refresh-token`, data, (err, response) => callback(err, response));
     } else {
       // If url is not set then request goes to oxd-server
-      utilities.oxdSocketRequest(data.port, data.host, data, 'get_access_token_by_refresh_token', function (err, response) {
-        return callback(err, response);
-      });
+      utilities.oxdSocketRequest(data.port, data.host, data, 'get_access_token_by_refresh_token', (err, response) => callback(err, response));
     }
   };
 
   /**
    * Function to get the information about the user using the access code obtained from the OP
    * @param {object} param - All required properties
-   * @returns {function} callback - Callback response function.
+   * @param {function} callback - Callback response function. It return with two parameters.
+   * {object} err - error response
+   * {object} response - success response
    */
-  module.get_user_info = function get_user_info(param, callback) {
+  module.get_user_info = (param, callback) => {
     // Filtered parameters
-    var data = filterParameters(param);
+    const data = filterParameters(param);
 
     // If url is set the request goes to oxd-https-extension
     if (!!data.url) {
-      utilities.oxdHttpRequest(data.url + '/get-user-info', data, function (err, response) {
-        return callback(err, response);
-      });
+      utilities.oxdHttpRequest(`${data.url}/get-user-info`, data, (err, response) => callback(err, response));
     } else {
       // If url is not set then request goes to oxd-server
-      utilities.oxdSocketRequest(data.port, data.host, data, 'get_user_info', function (err, response) {
-        return callback(err, response);
-      });
+      utilities.oxdSocketRequest(data.port, data.host, data, 'get_user_info', (err, response) => callback(err, response));
     }
   };
 
   /**
    * Function to logout the user
    * @param {object} param - All required properties
-   * @returns {function} callback - Callback response function.
+   * @param {function} callback - Callback response function. It return with two parameters.
+   * {object} err - error response
+   * {object} response - success response
    */
-  module.get_logout_uri = function get_logout_uri(param, callback) {
+  module.get_logout_uri = (param, callback) => {
     // Filtered parameters
-    var data = filterParameters(param);
+    const data = filterParameters(param);
 
     // If url is set the request goes to oxd-https-extension
     if (!!data.url) {
-      utilities.oxdHttpRequest(data.url + '/get-logout-uri', data, function (err, response) {
-        return callback(err, response);
-      });
+      utilities.oxdHttpRequest(`${data.url}/get-logout-uri`, data, (err, response) => callback(err, response));
     } else {
       // If url is not set then request goes to oxd-server
-      utilities.oxdSocketRequest(data.port, data.host, data, 'get_logout_uri', function (err, response) {
-        return callback(err, response);
-      });
+      utilities.oxdSocketRequest(data.port, data.host, data, 'get_logout_uri', (err, response) => callback(err, response));
     }
   };
 
   /**
    * Function to be used in a UMA Resource Server to protect resources.
    * @param {object} param - All required properties
-   * @returns {function} callback - Callback response function.
+   * @param {function} callback - Callback response function. It return with two parameters.
+   * {object} err - error response
+   * {object} response - success response
    */
-  module.uma_rs_protect = function uma_rs_protect(param, callback) {
+  module.uma_rs_protect = (param, callback) => {
     // Filtered parameters
-    var data = filterParameters(param);
+    const data = filterParameters(param);
 
     // If url is set the request goes to oxd-https-extension
     if (!!data.url) {
-      utilities.oxdHttpRequest(data.url + '/uma-rs-protect', data, function (err, response) {
-        return callback(err, response);
-      });
+      utilities.oxdHttpRequest(`${data.url}/uma-rs-protect`, data, (err, response) => callback(err, response));
     } else {
       // If url is not set then request goes to oxd-server
-      utilities.oxdSocketRequest(data.port, data.host, data, 'uma_rs_protect', function (err, response) {
-        return callback(err, response);
-      });
+      utilities.oxdSocketRequest(data.port, data.host, data, 'uma_rs_protect', (err, response) => callback(err, response));
     }
   };
 
   /**
    * Function to be used in a UMA Resource Server to check access.
    * @param {object} param - All required properties
-   * @returns {function} callback - Callback response function.
+   * @param {function} callback - Callback response function. It return with two parameters.
+   * {object} err - error response
+   * {object} response - success response
    */
-  module.uma_rs_check_access = function uma_rs_check_access(param, callback) {
+  module.uma_rs_check_access = (param, callback) => {
     // Filtered parameters
-    var data = filterParameters(param);
+    const data = filterParameters(param);
 
     // If url is set the request goes to oxd-https-extension
     if (!!data.url) {
-      utilities.oxdHttpRequest(data.url + '/uma-rs-check-access', data, function (err, response) {
-        return callback(err, response);
-      });
+      utilities.oxdHttpRequest(`${data.url}/uma-rs-check-access`, data, (err, response) => callback(err, response));
     } else {
       // If url is not set then request goes to oxd-server
-      utilities.oxdSocketRequest(data.port, data.host, data, 'uma_rs_check_access', function (err, response) {
-        return callback(err, response);
-      });
+      utilities.oxdSocketRequest(data.port, data.host, data, 'uma_rs_check_access', (err, response) => callback(err, response));
     }
   };
 
   /**
    * Function to be used by a UMA Requesting Party to get RPT token.
    * @param {object} param - All required properties
-   * @returns {function} callback - Callback response function.
+   * @param {function} callback - Callback response function. It return with two parameters.
+   * {object} err - error response
+   * {object} response - success response
    */
-  module.uma_rp_get_rpt = function uma_rp_get_rpt(param, callback) {
+  module.uma_rp_get_rpt = (param, callback) => {
     // Filtered parameters
-    var data = filterParameters(param);
+    const data = filterParameters(param);
 
     // If url is set the request goes to oxd-https-extension
     if (!!data.url) {
-      utilities.oxdHttpRequest(data.url + '/uma-rp-get-rpt', data, function (err, response) {
-        return callback(err, response);
-      });
+      utilities.oxdHttpRequest(`${data.url}/uma_rp_get_rpt`, data, (err, response) => callback(err, response));
     } else {
       // If url is not set then request goes to oxd-server
-      utilities.oxdSocketRequest(data.port, data.host, data, 'uma_rp_get_rpt', function (err, response) {
-        return callback(err, response);
-      });
+      utilities.oxdSocketRequest(data.port, data.host, data, 'uma_rp_get_rpt', (err, response) => callback(err, response));
     }
   };
 
   /**
    * Function to get the authorization url that can be opened in the browser for the user to provide authorization and authentication
    * @param {object} param - All required properties
-   * @returns {function} callback - Callback response function.
+   * @param {function} callback - Callback response function. It return with two parameters.
+   * {object} err - error response
+   * {object} response - success response
    */
-  module.uma_rp_get_claims_gathering_url = function uma_rp_get_claims_gathering_url(param, callback) {
+  module.uma_rp_get_claims_gathering_url = (param, callback) => {
     // Filtered parameters
-    var data = filterParameters(param);
+    const data = filterParameters(param);
 
     // If url is set the request goes to oxd-https-extension
     if (!!data.url) {
-      utilities.oxdHttpRequest(data.url + '/uma-rp-get-claims-gathering-url', data, function (err, response) {
-        return callback(err, response);
-      });
+      utilities.oxdHttpRequest(`${data.url}/uma-rp-get-claims-gathering-url`, data, (err, response) => callback(err, response));
     } else {
       // If url is not set then request goes to oxd-server
-      utilities.oxdSocketRequest(data.port, data.host, data, 'uma_rp_get_claims_gathering_url', function (err, response) {
-        return callback(err, response);
-      });
+      utilities.oxdSocketRequest(data.port, data.host, data, 'uma_rp_get_claims_gathering_url', (err, response) => callback(err, response));
     }
   };
 
   // Filter parameters
   function filterParameters(request) {
-    var data = config;
-    Object.keys(request).forEach(function (key) {
+    const data = config;
+    Object.keys(request).forEach((key) => {
       data[key] = request[key];
     });
 
--- oxd-node/utility.js
+++ oxd-node/utility.js
@@ -1,5 +1,5 @@
-var net = require('net');
-var httpRequest = require('request');
+const net = require('net');
+const httpRequest = require('request');
 
 /**
  * Function for oxd-server socket manipulation
@@ -7,53 +7,52 @@ var httpRequest = require('request');
  * @param {int} host - URL of the oxd-server
  * @param {object} params - List of parameters to request oxd-server command
  * @param {string} command - OXD Command name
- * @returns {function} callback - Callback response function
+ * @param {function} callback - Callback response function
  */
 function oxdSocketRequest(port, host, params, command, callback) {
   // OXD data
-  var data = {
+  let data = {
     command: command,
     params: params
   };
 
   // Create socket object
-  var client = new net.Socket();
+  const client = new net.Socket();
 
   // Initiate a connection on a given socket.
-  client.connect(port, host, function () {
+  client.connect(port, host, () => {
     data = JSON.stringify(data);
     console.log('Connected');
     try {
       if (data.length > 0 && data.length <= 100) {
-        console.log('Send data : ' + ('00' + data.length + data));
-        client.write(('00' + data.length + data));
-      }
-      else if (data.length > 100 && data.length < 1000) {
-        console.log('Send data : ' + ('0' + data.length + data));
-        client.write(('0' + data.length + data));
+        console.log(`Send data : 00${data.length + data}`);
+        client.write(`00${data.length + data}`);
+      } else if (data.length > 100 && data.length < 1000) {
+        console.log(`Send data : 0${data.length + data}`);
+        client.write(`0${data.length + data}`);
       }
     } catch (err) {
-      console.log('Send data error:' + err);
+      console.log('Send data error:', err);
     }
   });
 
   // Emitted when data is received
-  client.on('data', function (req) {
-    var data = req.toString();
-    console.log('Response : ' + data);
+  client.on('data', (req) => {
+    data = req.toString();
+    console.log('Response : ', data);
     callback(null, JSON.parse(data.substring(4, data.length)));
     client.end(); // kill client after server's response
   });
 
   // Emitted when an error occurs. The 'close' event will be called directly following this event.
-  client.on('error', function (err) {
-    console.log('Error: ' + err);
+  client.on('error', (err) => {
+    console.log('Error: ', err);
     callback(err, null);
     client.end(); // kill client after server's response
   });
 
   // Emitted when the server closes.
-  client.on('close', function () {
+  client.on('close', () => {
     console.log('Connection closed');
   });
 }
@@ -62,14 +61,13 @@ function oxdSocketRequest(port, host, params, command, callback) {
  *
  * @param {string} url - URL of oxd-https-extension with particular command request
  * @param {object} params - List of parameters to request oxd-server command
- * @returns {function} callback - Callback response function
+ * @param {function} callback - Callback response function
  */
 function oxdHttpRequest(url, params, callback) {
-
-  process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0'; //This line is to over come self signed certificate request issue, in real time this should be removed
+  process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0'; // This line is to over come self signed certificate request issue, in real time this should be removed
 
   // oxd-https-extension request
-  var options = {
+  const options = {
     url: url,
     body: params,
     json: true
@@ -77,26 +75,26 @@ function oxdHttpRequest(url, params, callback) {
 
   // Set protection_access_token in header
   if (params.protection_access_token != null) {
-    options['headers'] = {
-      Authorization: 'Bearer ' + params.protection_access_token
+    options.headers = {
+      Authorization: `Bearer ${params.protection_access_token}`
     };
   }
 
-  console.log('Request :' + JSON.stringify(options));
+  console.log('Request :', JSON.stringify(options));
 
   // http request
-  httpRequest.post(options, function (error, response, body) {
+  httpRequest.post(options, (error, response, body) => {
     if (!!error) {
-      console.log('Error: ' + error);
+      console.log('Error: ', error);
       return callback(error, null);
     }
 
-    if (!error && response.statusCode == 200) {
-      console.log('Response :' + JSON.stringify(body));
+    if (!error && response.statusCode === 200) {
+      console.log('Response :', JSON.stringify(body));
       return callback(null, body);
     }
 
-    console.log('Fail Response :' + JSON.stringify(body));
+    console.log('Fail Response :', JSON.stringify(body));
     return callback(body, null);
   });
 }
